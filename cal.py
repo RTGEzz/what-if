0409f4463014dedc8885eadb53a734e85672a577
RTGEzz <152403061+RTGEzz@users.noreply.github.com>
2025-04-19 14:03:27 +0700

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,24 @@
+import math
+
+while True:
+    print("Welcome to the calculator")
+    number1=int(input("Enter number 1  or exit :"))
+    if number1 == 'exit':
+        print("this program is stop")
+        break
+    else:
+        number2=int(input("Enter number 2 :"))
+        op=input("Enter operator (+ - * /)")
+
+        if op == '+':
+            total=number1 +number2
+            print(total)
+        elif op == '-':
+            total=number1-number2
+            print(total)
+        elif op == '*':
+            total=number1 * number2
+            print(total)
+        else:
+            total=number1 / number2
+            print(total)
